782962588b863e165476785316ebc4010b3d67a0
Tiago Pimentel Muniz <133404058+Guzhie@users.noreply.github.com>
2024-08-01 13:45:42 -0300

    termino da classe de conexao

--- mavenproject1/src/main/java/conexao/Conexao.java
+++ mavenproject1/src/main/java/conexao/Conexao.java
@@ -4,10 +4,52 @@
  */
 package conexao;
 
-/**
- *
- * @author Admin
- */
+import javax.swing.JOptionPane;
+import java.sql.*;
+
 public class Conexao {
+    final private String driver= "com.mysql.jdbc.Driver";
+    final private String url =  "jdbc://localhost/clientes":
+    final private String usuario = "root"; 
+    final private String senha = "";
+    private Connection conexao;
+    public Statement statement; 
+    public ResultSet resultset;
+            
+    public boolean conecta(){
+        boolean result = true;
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Conexão estabelecida" , "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch(ClassNotFoundException Driver){
+            JOptionPane.showMessageDialog(null, "Driver não localizado" +Driver,  "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+            result = false;
+        }
+        catch(SQLException Fonte){
+            JOptionPane.showMessageDialog(null, "Fonte de dados não localizada!"+Fonte, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+            result = false;
+        }
+    }
     
+    public void desconecta(){
+        try {
+            conexao.close();
+            JOptionPane.showMessageDialog(null, "Conexão com banco fechada","Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException fecha) {
+            JOptionPane.showMessageDialog(null, "Erro ao fechar o banco", "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    
+    public void executaSQL(String sql){
+        try {
+            statement = conexao.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            resultset = statement.executeQuery(sql);
+        } catch (SQLException excecao) {
+            JOptionPane.showMessageDialog(null, "Erro no comando SQL! \n Erro:" +excecao, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
 }
+        
