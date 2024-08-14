2a382c57e53d8fb9c34f2f0b97b96f1338f74045
Guzhie <133404058+Guzhie@users.noreply.github.com>
2024-08-14 16:01:08 -0300

    termino da sequencia 2

--- mavenproject1/src/main/java/controle/TabelaCli.java
+++ mavenproject1/src/main/java/controle/TabelaCli.java
@@ -1,134 +1,179 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controle;
 
 import java.awt.*;
 import java.text.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.text.MaskFormatter;
-
 import java.sql.*;
-
 import conexao.Conexao;
-import javax.swing.JOptionPane;
-
-import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.DefaultTableModel;
 
-
-public class TabelaCli extends JFrame{
+public class TabelaCli extends JFrame {
     
-        Conexao con_cliente;
+    Conexao con_cliente;
     
-        JLabel rCodigo, rNome, rEmail, rTel, rData;
-        JTextField tCodigo, tNome, tEmail;
-        JFormattedTextField tel, data;
-        MaskFormatter mTel,mData;
+    JButton bprimei, banterior, bproximo, bultimo;
+    JLabel rCodigo, rNome, rEmail, rTel, rData;
+    JTextField tCodigo, tNome, tEmail;
+    JFormattedTextField tel, data;
+    MaskFormatter mTel, mData;
+    
+    JTable tblClientes;
+    JScrollPane scp_tabela;
+    
+    public TabelaCli() {
+        con_cliente = new Conexao(); // inicialização do objeto
+        con_cliente.conecta(); // chama o método que conecta
         
-        JTable tblClientes;
-        JScrollPane scp_tabela;
+        Container tela = getContentPane();
+        setLayout(null);
+
+        setTitle("Conexão de Java com MySql");
+        setResizable(false);
         
-        public TabelaCli(){
-            con_cliente = new Conexao(); // inicialização do objeto
-            con_cliente.conecta(); // chama o método que conecta           
-            
-            Container tela = getContentPane();
-            setLayout(null);
+        try {
+            mData = new MaskFormatter("##/##/####");
+            mTel = new MaskFormatter("(##) ####-####");
+            mData.setPlaceholderCharacter('_');
+            mTel.setPlaceholderCharacter('_');
+        } catch (ParseException excp) {
+            excp.printStackTrace();
+        }
 
-            setTitle("Conexão de Java com MySql");
-            setResizable(false);
-         
-            try{
-                mData = new MaskFormatter("##/##/###");
-                mTel = new MaskFormatter("(##)####-####");
-                mData.setPlaceholderCharacter('_');
-                mTel.setPlaceholderCharacter('_');
+        rCodigo = new JLabel("Codigo:");
+        rCodigo.setBounds(30, 20, 100, 20);
+        tCodigo = new JTextField(50);
+        tCodigo.setBounds(120, 20, 200, 25);
 
-            } catch(ParseException excp){}    
-
-            rCodigo = new JLabel("Codigo:");
-            rCodigo.setBounds(30, 20, 100, 20);
-            tCodigo = new JTextField(50);
-            tCodigo.setBounds(120, 20, 200, 25);
+        rNome = new JLabel("Nome:");
+        rNome.setBounds(30, 50, 100, 20);
+        tNome = new JTextField(50);
+        tNome.setBounds(120, 50, 200, 25);
         
+        rData = new JLabel("Data de Nascimento:");
+        rData.setBounds(30, 80, 150, 20);
+        data = new JFormattedTextField(mData);
+        data.setBounds(150, 80, 150, 20);
         
-            rNome = new JLabel("Nome:");
-            rNome.setBounds(30, 50, 100, 20);
-            tNome = new JTextField(50);
-            tNome.setBounds(120, 50, 200, 25);
-            
-            rData = new JLabel("Data de Nascimento:");
-            rData.setBounds(30, 80, 150, 20);
-            data = new JFormattedTextField(mData);
-            data.setBounds(150, 80, 150, 20);
-            
-            rTel = new JLabel("Telefone:");
-            rTel.setBounds(30, 110, 100, 20);
-            tel = new JFormattedTextField(mTel);
-            tel.setBounds(120, 110, 150, 20);
+        rTel = new JLabel("Telefone:");
+        rTel.setBounds(30, 110, 100, 20);
+        tel = new JFormattedTextField(mTel);
+        tel.setBounds(120, 110, 150, 20);
 
-            
-            
-            rEmail = new JLabel("Email:");
-            rEmail.setBounds(30, 140, 100, 20);
-            tEmail = new JTextField(50);
-            tEmail.setBounds(120, 140, 200, 25);
-            
-            tela.add(rCodigo);
-            tela.add(tCodigo);
-            tela.add(rNome);
-            tela.add(tNome);
-            tela.add(rData);
-            tela.add(data);
-            tela.add(rTel);
-            tela.add(tel);
-            tela.add(rEmail);
-            tela.add(tEmail);
-            
-            tblClientes = new javax.swing.JTable();
-            scp_tabela = new javax.swing.JScrollPane();
-            
-            tblClientes.setBounds(50, 200,550, 200);
-            scp_tabela.setBounds(50, 200,550, 200);
-            
-            tela.add(tblClientes);
-            tela.add(scp_tabela);
-            
-            tblClientes.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0,0,0)));
-            
-            tblClientes.setFont(new java.awt.Font("Arial", 1, 12));
-            
-            tblClientes.setModel(new javax.swing.table.DefaultTableModel(
+        rEmail = new JLabel("Email:");
+        rEmail.setBounds(30, 140, 100, 20);
+        tEmail = new JTextField(50);
+        tEmail.setBounds(120, 140, 200, 25);
+        
+        bprimei = new JButton("Primeiro");
+        bprimei.setBounds(30, 180, 100, 20);
+        bprimei.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    con_cliente.resultset.first();
+                    mostrar_Dados();
+                } catch (SQLException erro) {
+                    JOptionPane.showMessageDialog(null, "Não foi possível acessar o primeiro registro: " + erro.getMessage());
+                }
+            }
+        });
+
+        banterior = new JButton("Anterior");
+        banterior.setBounds(130, 180, 100, 20);
+        banterior.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (con_cliente.resultset.previous()) {
+                        mostrar_Dados();
+                    }
+                } catch (SQLException erro) {
+                    JOptionPane.showMessageDialog(null, "Não foi possível acessar o registro anterior: " + erro.getMessage());
+                }
+            }
+        });
+
+        bproximo = new JButton("Próximo");
+        bproximo.setBounds(230, 180, 100, 20);
+        bproximo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (con_cliente.resultset.next()) {
+                        mostrar_Dados();
+                    }
+                } catch (SQLException erro) {
+                    JOptionPane.showMessageDialog(null, "Não foi possível acessar o próximo registro: " + erro.getMessage());
+                }
+            }
+        });
+
+        bultimo = new JButton("Último");
+        bultimo.setBounds(330, 180, 100, 20);
+        bultimo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    con_cliente.resultset.last();
+                    mostrar_Dados();
+                } catch (SQLException erro) {
+                    JOptionPane.showMessageDialog(null, "Não foi possível acessar o último registro: " + erro.getMessage());
+                }
+            }
+        });
+        
+        tela.add(rCodigo);
+        tela.add(tCodigo);
+        tela.add(rNome);
+        tela.add(tNome);
+        tela.add(rData);
+        tela.add(data);
+        tela.add(rTel);
+        tela.add(tel);
+        tela.add(rEmail);
+        tela.add(tEmail);
+        tela.add(bprimei);
+        tela.add(banterior);
+        tela.add(bproximo);
+        tela.add(bultimo);
+        
+        tblClientes = new JTable();
+        scp_tabela = new JScrollPane(tblClientes);
+        
+        scp_tabela.setBounds(50, 210, 550, 200);
+        
+        tela.add(scp_tabela);
+        
+        tblClientes.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        tblClientes.setFont(new Font("Arial", Font.BOLD, 12));
+        
+        tblClientes.setModel(new DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null},
             },
-            new String[] {"Codigo", "Nome", "Data Nascimento", "Telefone", "Email"})
-            {
-            boolean[] canEdit = new boolean[]{false,false,false,false,false};
-            
-            public boolean isCellEditable(int rowIndex, int columnIndex){
-            return canEdit[columnIndex];}
-            }
-            );
-            scp_tabela.setViewportView(tblClientes);
-            
-            tblClientes.setAutoCreateRowSorter(true);
-            
-            setSize(800,600);
-            setVisible(true);
-            setLocationRelativeTo(null);
-            
-            con_cliente.executaSQL("select * from tbclientes order by cod");
-            preencherTabela();
-        }
-        
+            new String[] {"Codigo", "Nome", "Data Nascimento", "Telefone", "Email"}
+        ) {
+            boolean[] canEdit = new boolean[]{false, false, false, false, false};
 
-        public void preencherTabela() {
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit[columnIndex];
+            }
+        });
+        
+        tblClientes.setAutoCreateRowSorter(true);
+        scp_tabela.setViewportView(tblClientes);
+        
+        setSize(800, 600);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        
+        con_cliente.executaSQL("SELECT * FROM tbclientes ORDER BY cod");
+        preencherTabela();
+    }
+    
+    public void preencherTabela() {
         tblClientes.getColumnModel().getColumn(0).setPreferredWidth(4);
         tblClientes.getColumnModel().getColumn(1).setPreferredWidth(150);
         tblClientes.getColumnModel().getColumn(2).setPreferredWidth(11);
@@ -137,7 +182,7 @@ public class TabelaCli extends JFrame{
 
         DefaultTableModel modelo = (DefaultTableModel) tblClientes.getModel();
         modelo.setNumRows(0);
-            
+        
         try {
             con_cliente.resultset.beforeFirst();
             while (con_cliente.resultset.next()) {
@@ -150,9 +195,19 @@ public class TabelaCli extends JFrame{
                 });
             }
         } catch (SQLException erro) {
-            JOptionPane.showMessageDialog(null, "Erro ao listar dados da tabela!! :\n" + erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Erro ao listar dados da tabela: " + erro.getMessage(), "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
         }
     }
-
-        
+    
+    public void mostrar_Dados() {
+        try {
+            tCodigo.setText(con_cliente.resultset.getString("cod"));
+            tNome.setText(con_cliente.resultset.getString("nome"));
+            data.setText(con_cliente.resultset.getString("dt_nasc"));
+            tel.setText(con_cliente.resultset.getString("telefone"));
+            tEmail.setText(con_cliente.resultset.getString("email"));
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Não localizou dados: " + erro.getMessage(), "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
 }
